fdef2458dcc8e00914d7bd18bb9761c5992df831
Caroline Blank <caro@c-space.org>
2023-09-08 17:33:38 +0200

    Ajout de la classe LatexComponent avec envoi d'un POST au serveur qui renvoie du texte.

--- server/tdoc/app/urls.py
+++ server/tdoc/app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("render-latex", views.renderlatex, name="renderlatex"),
     # path("defaults", views.defaults, name="defaults"),
     # path("submit", views.submit, name="submit"),
 ]
--- server/tdoc/app/views.py
+++ server/tdoc/app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def index(request):
-    return render(request, 'mockup.html')
+    return render(request, 'testWebComponent.html');
+
+def renderlatex(request):
+    return HttpResponse('Hello Latex');
