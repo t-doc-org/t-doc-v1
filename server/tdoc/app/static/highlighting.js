288b37051a4b08fe915d3ab08a5451ff4a7dac12
Caroline Blank <caro@c-space.org>
2023-11-10 14:27:11 +0100

    Ajout de la numérotation des lignes de code.

--- server/tdoc/app/static/highlighting.js
+++ server/tdoc/app/static/highlighting.js
@@ -1,11 +1,16 @@
 import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', async (event) => {
     const css = document.createElement("link");
     css.rel = "stylesheet"
     css.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css";
     css.async = true;
     document.head.appendChild(css);
+
+    // Ajout de hljs aux variables globales pour pouvoir y accéder et patcher highlight.js avec le nouveau module grâce à un import dynamique
+    window.hljs = hljs;
+    await import('https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js@2.8.0/dist/highlightjs-line-numbers.min.js');
+
     document.querySelectorAll('pre.verbatim').forEach((el) => {
         const text = el.innerHTML;
         const re = /^\{([a-zA-Z0-9_-]+)\}( *\n)?/;
@@ -14,8 +19,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const lang = m[1];
             el.innerHTML = text.substr(m[0].length);
             el.classList.add(`language-${lang}`);
-            console.log(lang);
         }
         hljs.highlightElement(el);
+        hljs.lineNumbersBlock(el);
     });
 });
