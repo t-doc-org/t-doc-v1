dc31468b82bcf45f1cb499bdc57d7532163b1eb8
Caroline Blank <caro@c-space.org>
2023-11-23 13:14:11 +0100

    Fait le ménage.
     - Ajouté des docstrings.
     - Supprimé les commentaires et docstrings inutiles.
     - Formaté le code de manière consistante.

--- server/tdoc/app/views.py
+++ server/tdoc/app/views.py
@@ -1,9 +1,3 @@
-from django.conf import settings
-from django.core.cache import cache
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-
 import hashlib
 import html
 import mimetypes
@@ -12,20 +6,38 @@ import re
 import struct
 import subprocess
 
+from django.conf import settings
+from django.core.cache import cache
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+
 def random_name(base):
+    """Generate a random base name for the rendering output."""
     num = struct.unpack("I", os.urandom(4))[0]
     return f"{base}-{num}"
 
-#use to delete ANSI escape code in error message
-ansiRE = re.compile(rb"\x1b\[[\x30-\x3f]*[\x20-\x2f]*[\x40-\x7e]")
 
-def hash_varint(m, value):
+# Match ANSI escape codes.
+ansi_re = re.compile(rb"\x1b\[[\x30-\x3f]*[\x20-\x2f]*[\x40-\x7e]")
+
+
+def remove_ansi_escapes(s):
+    """Remove ANSI escape codes from the given string."""
+    return ansi_re.sub(b"", s)
+
+
+def hash_varint(h, value):
+    """Hash the varint representation of an integer."""
     while True:
-        m.update(b'%c' % ((value & 0x7f) | (0x80 if value > 0x7f else 0)))
+        h.update(b'%c' % ((value & 0x7f) | (0x80 if value > 0x7f else 0)))
         value >>= 7
         if value == 0: break
 
+
 def generate_key(latex, config, version):
+    """Generate a hash key for an input file and the rendering config."""
     m = hashlib.sha256()
     hash_varint(m, len(latex))
     m.update(latex)
@@ -35,15 +47,28 @@ def generate_key(latex, config, version):
     m.update(version)
     return f"latex:{m.hexdigest()}"
 
+
 class RenderException(Exception):
+    """An exception raised when LaTeX rendering fails."""
     def __init__(self, msg, output):
         super().__init__(msg)
         self.output = output
 
 
+# Match a <link> tag referencing a specific CSS file.
 css_re = r"<link [^>]*href=[\"']%s\.css[\"'][^>]*>"
 
+
+def inline_css_link(doc, name, css):
+    """Replace a <link> tag referencing a CSS file with its content."""
+    # <link href='output-2687784357.css' rel='stylesheet' type='text/css' />
+    return re.sub(css_re % re.escape(name),
+                  '<style type="text/css">' + html.escape(css) + "</style>\n",
+                  doc)
+
+
 def doc(request, file):
+    """Handle the rendering of a LaTeX document."""
     name = random_name("output")
     latex = (settings.TEX_ROOT / f"{file}.tex").read_bytes()
     configpath = settings.TEX_ROOT / "latex.cfg"
@@ -64,11 +89,9 @@ def doc(request, file):
                                  env=env)
             if res.returncode != 0:
                 raise RenderException("failed to render", res.stdout)
-            css = (settings.TMP / f"{name}.css").read_text()
             content = (settings.TMP / f"{name}.html").read_text()
-                # <link href='output-2687784357.css' rel='stylesheet' type='text/css' />
-            content = re.sub(css_re % re.escape(name), '<style type="text/css">' + html.escape(css) + "</style>\n", content)
-            return content
+            css = (settings.TMP / f"{name}.css").read_text()
+            return inline_css_link(content, name, css)
         finally:
             for path in settings.TMP.glob(f"{name}*"):
                 path.unlink()
@@ -77,12 +100,14 @@ def doc(request, file):
         htmlresp = cache.get_or_set(key, render)
         return HttpResponse(htmlresp, content_type="text/html")
     except RenderException as e:
-        return HttpResponse(ansiRE.sub(b"", e.output), status=400, content_type="text/plain")
+        return HttpResponse(remove_ansi_escapes(e.output), status=400,
+                            content_type="text/plain")
+
 
 def image(request, image):
+    """Handle the retrieval of images."""
     try:
         img = (settings.IMG_ROOT / f"{image}").read_bytes()
         return HttpResponse(img, content_type=mimetypes.guess_type(image)[0])
     except IOError as e:
         return HttpResponse(str(e), status=400, content_type="text/plain")
-
