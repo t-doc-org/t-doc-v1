8574376bb2c26d27e1b24ff7af147a563dfd5d93
Caroline Blank <caro@c-space.org>
2023-11-23 17:49:32 +0100

    Utilisation d'un namespace dans le CSS, pour éviter les conflits.

--- server/tdoc/app/static/solutions.js
+++ server/tdoc/app/static/solutions.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   // Add buttons to all solution sections.
-  document.querySelectorAll('div.response').forEach((el) => {
+  document.querySelectorAll('div.tdoc-answer').forEach((el) => {
     // Add a button before each solution section. The button toggles the
     // visibility of the section.
     const btn = document.createElement("button");
-    btn.className = "solution";
+    btn.className = "tdoc-solution";
     btn.textContent = "Solution";
     btn.addEventListener("click", function () {
-      el.classList.toggle("hide");
+      el.classList.toggle("tdoc-hidden");
     });
     el.before(btn);
   });
