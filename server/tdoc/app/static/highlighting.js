cf69678a523a24ea9ec6c5c0099ee6e4595705b3
Caroline Blank <caro@c-space.org>
2023-12-08 16:25:00 +0100

    Nouvelle fonctionnalité: le code python peut être exécuté.

--- server/tdoc/app/static/highlighting.js
+++ server/tdoc/app/static/highlighting.js
@@ -16,17 +16,63 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     await import('https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js' +
                  '@2.8.0/dist/highlightjs-line-numbers.min.js');
 
+    // Load Skulpt and its standard library.
+    const skulpt = document.createElement('script');
+    skulpt.src = 'https://skulpt.org/js/skulpt.min.js';
+    skulpt.type = 'text/javascript';
+    document.head.appendChild(skulpt);
+    skulpt.addEventListener('load', () => {
+        const skulpt_stdlib = document.createElement('script');
+        skulpt_stdlib.src = 'https://skulpt.org/js/skulpt-stdlib.js';
+        skulpt_stdlib.type = 'text/javascript';
+        document.head.appendChild(skulpt_stdlib);
+    });
+
     // Highlight all verbatim sections.
     document.querySelectorAll('pre.verbatim').forEach((el) => {
         // Extract an optional language identifier prefix of the form
-        // "{python}".
+        // "{python}" or "{python,interactive}.
         const text = el.innerHTML;
-        const re = /^\{([a-zA-Z0-9_-]+)\}( *\n)?/;
+        const re = /^\{([a-zA-Z0-9_,-]+)\}( *\n)?/;
         const m = re.exec(text);
         if (m) {  // Found, remove the prefix and add the relevant CSS class
-            const lang = m[1];
-            el.innerHTML = text.substr(m[0].length);
+            const lang = m[1].split(",")[0];
+            const inter = m[1].split(",")[1];
+            // Display code
+            const code = text.substr(m[0].length);
+            el.innerHTML = code;
             el.classList.add(`language-${lang}`);
+            // Add a button to execute code
+            if (inter && lang == "python") {
+                const button = document.createElement("button");
+                button.innerHTML = "Exécuter";
+                el.after(button);
+                // Add a pre to display the result
+                const pre = document.createElement("pre");
+                pre.innerHTML = "Le résultat de l'exécution sera affiché ici.";
+                button.after(pre);
+
+                button.addEventListener('click', async (event) => {
+                    pre.innerHTML = "";
+                    function outf(text) {
+                        pre.innerHTML += text;
+                    }
+                    function builtinRead(x) {
+                        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+                                throw "File not found: '" + x + "'";
+                        return Sk.builtinFiles["files"][x];
+                    }
+                    Sk.configure({output: outf, read: builtinRead});
+                    await Sk.misceval.asyncToPromise(function() {
+                        // convert code in html to text
+                        const c = code.replaceAll('&nbsp;', ' ')
+                                      .replaceAll('&lt;', '<')
+                                      .replaceAll('&gt;', '>')
+                                      .replaceAll('&amp;', '&');
+                        return Sk.importMainWithBody("<stdin>", false, c, true);
+                   });
+                });
+            }
         }
         hljs.highlightElement(el);
         hljs.lineNumbersBlock(el);
