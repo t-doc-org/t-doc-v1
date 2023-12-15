ffe16ce29c7af80f44cdf89f8a33d5cb678af0df
Caroline Blank <caro@c-space.org>
2023-12-15 15:02:08 +0100

    Mise au propre.

--- server/tdoc/app/static/highlighting.js
+++ server/tdoc/app/static/highlighting.js
@@ -1,5 +1,12 @@
 import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/es/highlight.min.js';
 
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined ||
+        Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
 document.addEventListener('DOMContentLoaded', async (event) => {
     // Add the highlight.js stylesheet.
     const css = document.createElement('link');
@@ -16,11 +23,13 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     await import('https://cdn.jsdelivr.net/npm/highlightjs-line-numbers.js' +
                  '@2.8.0/dist/highlightjs-line-numbers.min.js');
 
-    // Load Skulpt and its standard library.
+    // Load Skulpt.
     const skulpt = document.createElement('script');
     skulpt.src = 'https://skulpt.org/js/skulpt.min.js';
     skulpt.type = 'text/javascript';
     document.head.appendChild(skulpt);
+    // Because the standard library depends on Skulpt,
+    // we wait until Skulpt loaded
     skulpt.addEventListener('load', () => {
         const skulpt_stdlib = document.createElement('script');
         skulpt_stdlib.src = 'https://skulpt.org/js/skulpt-stdlib.js';
@@ -28,61 +37,51 @@ document.addEventListener('DOMContentLoaded', async (event) => {
         document.head.appendChild(skulpt_stdlib);
     });
 
-    // Highlight all verbatim sections.
+    // Manage code (display or add possibility to execute) and highlight all
+    // verbatim sections.
     document.querySelectorAll('pre.verbatim').forEach((el) => {
         // Extract an optional language identifier prefix of the form
         // "{python}" or "{python,interactive}.
         const text = el.innerHTML;
         const re = /^\{([a-zA-Z0-9_,-]+)\}( *\n)?/;
         const m = re.exec(text);
-        if (m) {  // Found, remove the prefix and add the relevant CSS class
-            const lang = m[1].split(",")[0];
-            const inter = m[1].split(",")[1];
-            // Display code
-            const code = text.substr(m[0].length);
+        if (m) {
+            const [lang, inter] = m[1].split(",");
+            // Display code.
+            const code = text.substr(m[0].length);  // Remove the prefix
             el.innerHTML = code;
-            el.classList.add(`language-${lang}`);
-            // Add a button to execute code
-            if (inter && lang == "python") {
+            el.classList.add(`language-${lang}`);  // Set class for hljs
+            // Add a button to execute code.
+            if (lang == "python" && inter == "interactive") {
                 const button = document.createElement("button");
                 button.innerHTML = "Exécuter";
                 el.after(button);
-                // Add a pre to display the result
+                // Add a pre to display the result.
                 const pre = document.createElement("pre");
-                pre.innerHTML = "";
                 pre.className = "tdoc-execution";
                 button.after(pre);
 
                 button.addEventListener('click', async (event) => {
-                    function outf(text) {
-                        pre.innerHTML += text;
-                    }
-                    function builtinRead(x) {
-                        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-                                throw "File not found: '" + x + "'";
-                        return Sk.builtinFiles["files"][x];
-                    }
+                    // Code execution
                     if (button.innerHTML == "Exécuter") {
                         button.innerHTML = "Effacer";
                         pre.style.display = 'block';
                         Sk.configure({
-                            inputfun: function (prompt) {
-                                return window.prompt(prompt);
-                            },
+                            inputfun: (prompt) => window.prompt(prompt),
                             inputfunTakesPrompt: true,
-                            output: outf,
+                            output: (text) => { pre.innerHTML += text },
                             read: builtinRead
                         });
-                        //Sk.configure({output: outf, read: builtinRead});
-                        await Sk.misceval.asyncToPromise(function() {
-                            // convert code in html to text
+                        await Sk.misceval.asyncToPromise(() => {
+                            // Convert code in HTML to text.
                             const c = code.replaceAll('&nbsp;', ' ')
                                           .replaceAll('&lt;', '<')
                                           .replaceAll('&gt;', '>')
                                           .replaceAll('&amp;', '&');
-                            return Sk.importMainWithBody("<stdin>", false, c, true);
+                            return Sk.importMainWithBody("<stdin>", false, c,
+                                                         true);
                        });
-                    } else {
+                    } else {  // Clear
                         button.innerHTML = "Exécuter";
                         pre.innerHTML = "";
                         pre.style.display = 'none';
