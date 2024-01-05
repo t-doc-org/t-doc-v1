37a91ac1023f79607256241d45d0af5c72611d90
Caroline Blank <caro@c-space.org>
2024-01-05 16:46:32 +0100

    Changement du block code, les arguments deviennent des classes dans le HTML et ne font plus partie du code.

--- server/tdoc/app/static/code.js
+++ server/tdoc/app/static/code.js
@@ -40,74 +40,76 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     // Manage code (display or add possibility to execute) and highlight all
     // code sections.
     document.querySelectorAll('div.tdoc-code > span').forEach((el) => {
-        // Extract an optional language identifier prefix of the form
-        // "{python}" or "{python,interactive}.
-        const text = el.innerHTML.replaceAll(' ', ''); // à corriger
-        const re = /^\{([a-zA-Z0-9_,-]+)\}( *\n)?/;
-        const m = re.exec(text);
-        if (m) {
-            const [lang, inter, turt] = m[1].split(",");
-            // Display code.
-            const code = text.substr(m[0].length);  // Remove the prefix
-            el.innerHTML = code;
-            el.classList.add(`language-${lang}`);  // Set class for hljs
-            // Add a button to execute code.
-            if (lang == "python" && inter == "interactive") {
-                // Convert code in HTML to text.
-                const code_text = code.replaceAll('&nbsp;', ' ')
-                                      .replaceAll('&lt;', '<')
-                                      .replaceAll('&gt;', '>')
-                                      .replaceAll('&amp;', '&');
-                const button = document.createElement("button");
-                button.innerHTML = "Exécuter";
-                el.after(button);
-                // Add a pre to display the result.
-                let pre;
-                let turtle;
-
-                button.addEventListener('click', async (event) => {
-                    // Code execution
-                    if (button.innerHTML == "Exécuter") {
-                        button.innerHTML = "Effacer";
-                        if (turt == "turtle") {
-                            turtle = document.createElement("div");
-                            turtle.className = "tdoc-turtle";
-                            button.after(turtle);
-                            Sk.TurtleGraphics = {target: turtle};
-                            Sk.TurtleGraphics.width = 800;
-                            Sk.TurtleGraphics.height = 500;
-                        }
-
-                        Sk.configure({
-                            inputfun: (prompt) => window.prompt(prompt),
-                            inputfunTakesPrompt: true,
-                            output: (text) => {
-                                if (!pre && text != "") {
-                                    pre = document.createElement("pre");
-                                    pre.className = "tdoc-execution";
-                                    button.after(pre);
-                                }
-                                pre.innerHTML += text;
-                            },
-                            read: builtinRead
-                        });
-                        await Sk.misceval.asyncToPromise(() => {
-                            return Sk.importMainWithBody("<stdin>", false,
-                                                         code_text, true);
-                       });
-                    } else {  // Clear
-                        button.innerHTML = "Exécuter";
-                        if (pre) {
-                            pre.remove();
-                            pre = undefined;
-                        }
-                        if (turtle) {
-                            turtle.remove();
-                            turtle = undefined;
-                        }
-                    }
-                });
+        // Determine the code language and any options.
+        let lang, interactive = false, turtle = false;
+        el.parentNode.classList.forEach((c) => {
+            if (c.startsWith("tdoc-lang-")) lang = c.substring(10);
+            if (c.startsWith("tdoc-code-")) {
+                if (c.includes("interactive")) interactive = true;
+                if (c.includes("turtle")) turtle = true;
             }
+        });
+
+        // Remove leading spaces that are sometimes added by LaTeX.
+        const code = el.innerHTML.replaceAll(' ', ''); // à corriger
+        el.innerHTML = code;
+        el.classList.add(`language-${lang}`);  // Set class for hljs
+
+        // Add a button to execute code.
+        if (lang == "python" && interactive) {
+            // Convert code in HTML to text.
+            const code_text = code.replaceAll('&nbsp;', ' ')
+                                  .replaceAll('&lt;', '<')
+                                  .replaceAll('&gt;', '>')
+                                  .replaceAll('&amp;', '&');
+            const button = document.createElement("button");
+            button.innerHTML = "Exécuter";
+            el.after(button);
+
+            // Add a pre to display the result.
+            let pre, turtleDiv;
+            button.addEventListener('click', async (event) => {
+                // Code execution
+                if (button.innerHTML == "Exécuter") {
+                    button.innerHTML = "Effacer";
+                    if (turtle) {
+                        turtleDiv = document.createElement("div");
+                        turtleDiv.className = "tdoc-turtle";
+                        button.after(turtleDiv);
+                        Sk.TurtleGraphics = {target: turtleDiv};
+                        Sk.TurtleGraphics.width = 800;
+                        Sk.TurtleGraphics.height = 500;
+                    }
+
+                    Sk.configure({
+                        inputfun: (prompt) => window.prompt(prompt),
+                        inputfunTakesPrompt: true,
+                        output: (text) => {
+                            if (!pre && text != "") {
+                                pre = document.createElement("pre");
+                                pre.className = "tdoc-execution";
+                                button.after(pre);
+                            }
+                            pre.innerHTML += text;
+                        },
+                        read: builtinRead
+                    });
+                    await Sk.misceval.asyncToPromise(() => {
+                        return Sk.importMainWithBody("<stdin>", false,
+                                                     code_text, true);
+                   });
+                } else {  // Clear
+                    button.innerHTML = "Exécuter";
+                    if (pre) {
+                        pre.remove();
+                        pre = undefined;
+                    }
+                    if (turtleDiv) {
+                        turtleDiv.remove();
+                        turtleDiv = undefined;
+                    }
+                }
+            });
         }
         hljs.highlightElement(el);
         hljs.lineNumbersBlock(el);
